34a40987bf69d0485aaa871c19aa4d74d340f1da
Marly Magdy <119417723+marlymagdy@users.noreply.github.com>
2024-05-01 17:19:40 +0300

    huffman.py

--- huffman.py
+++ huffman.py
@@ -1,3 +1,4 @@
+import Functions
 import heapq
 from collections import Counter, defaultdict
 
@@ -38,25 +39,56 @@ def huffman_encode(message, mapping):
     encoded_message = ''.join(mapping[char] for char in message)
     return encoded_message
 
-def calculate_bits(message):
-    return len(message) * 8  # Assuming 8 bits per character
+def mapping_to_2d_array(mapping):
+    """
+    Convert mapping dictionary to a 2D array.
 
-def calculate_compression_ratio(before_bits, after_bits):
-    return before_bits / after_bits
+    Args:
+        mapping (dict): Mapping of characters to Huffman codes.
 
-# Example usage
-message = input("Enter your message here")
-tree = build_huffman_tree(message)
-mapping = build_huffman_mapping(tree)
-encoded_message = huffman_encode(message, mapping)
+    Returns:
+        list: 2D array containing characters and their Huffman codes.
+    """
+    mapping_array = []
+    for char, code in mapping.items():
+        mapping_array.append([char, int(code)])
+    return mapping_array
 
-before_bits = calculate_bits(message)
-after_bits = len(encoded_message)
 
-compression_ratio = calculate_compression_ratio(before_bits, after_bits)
 
-print("Original Message:", message)
-print("Encoded Message:", encoded_message)
-print("Total bits before encoding:", before_bits)
-print("Total bits after encoding:", after_bits)
-print("Compression ratio:", compression_ratio)
+def compression_with_huffman(message):
+
+    tree = build_huffman_tree(message)
+    mapping = build_huffman_mapping(tree)
+    print(mapping)
+    array_mapping=mapping_to_2d_array(mapping)
+    print(array_mapping)
+
+
+    prob_table=Functions.calculate_probabilities(message)
+
+    avg_length=Functions.calculate_average_length(mapping,prob_table)
+    entropy=Functions.calculate_entropy(message)
+    efficiency=(entropy/avg_length)*100
+
+    encoded_message = huffman_encode(message, mapping)  # results
+    bits_before=Functions.calculate_bits_before(message)
+    bits_after = len(encoded_message)
+
+    Compression_ratio = bits_before / bits_after
+
+
+
+    return {
+            "result": encoded_message,
+            "bits_before": bits_before,
+            "bits_after": bits_after,
+            "cr": Compression_ratio,
+            "entropy": entropy,
+            "avg_len": avg_length,
+            "efficiency": efficiency,
+            "prob_dict": prob_table
+        }
+v=compression_with_huffman("aaaabbccdf")
+#print(v)
+
